1f2ed9a5c68b01e4e6c4a6978706c7925da282ff
Shikitimiau <shikitimiau@comunidad.unam.mx>
2020-05-13 21:03:21 -0500

    Reinicio

--- archivin.php
+++ archivin.php
@@ -1,14 +1,12 @@
 <?php
-
+  // parte de Diego
   echo "Shikitimiau";
 
-
+ // parte de emi
 $variableDelEmi="Holaaaaaa, asjasjasdasd";
-
+ // parte de chong
   function hola (){
     echo "Hola";
   }
   hola();
-
-  echo "Shikitimiau";
 ?>
