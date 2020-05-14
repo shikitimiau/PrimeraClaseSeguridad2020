f829fba2f662e1fcbb3bbadeb4ef5b0aa4f9507b
AntChong1226 <antoniochong1820@comunidad.unam.mx>
2020-05-13 22:02:17 -0500

    feat: PI

--- archivin.php
+++ archivin.php
@@ -1,12 +1,3 @@
 <?php
-  // parte de Diego
-  echo "Shikitimiau";
-
- // parte de emi
-$variableDelEmi="Holaaaaaa, asjasjasdasd";
- // parte de chong
-  function hola (){
-    echo "Hola";
-  }
-  hola();
+  define("PI", 3.1416);
 ?>
